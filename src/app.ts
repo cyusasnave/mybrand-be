cc9e87fa3b415edd8a8d9e03948b5741f52f665a
CYUSA Snave <cyusasnaveee@gmail.com>
2024-03-28 08:54:16 +0200

    made touches

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 const corsOptions = {
   origin: [
-    "https://mybrand-be-asyh.onrender.com/",
+    "https://mybrand-be-asyh.onrender.com/api-docs/",
     "http://localhost:3000",
     "http://127.0.0.1:5501",
   ],
